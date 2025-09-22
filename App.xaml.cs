69afbca911f0328cda6f8ee5f54a05bf0cea48ce
Khasanova <starlightgleemer@gmail.com>
2025-09-22 12:15:58 +0500

    пробую связать

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace L_R1._1_Khasanova
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
